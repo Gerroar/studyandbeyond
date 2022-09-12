3098cfca0a70e743082ab18b7a32021dde964364
Gerroar <germanariasrodriguez@gmail.com>
2022-09-12 12:11:33 +0200

    Animaciones inciales listas

--- frontend/src/components/contentPages/Home.js
+++ frontend/src/components/contentPages/Home.js
@@ -1,22 +1,57 @@
 import { ReactComponent as SBLogo } from "../../img/sb-logo-white-buttons.svg";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 export default function Home() {
   /**Aqui se muestran los botones principales con información de SB y
    * porque estudiar en Dinamarca
    */
+
+  const sbDiv = { 
+    visible: {
+      marginLeft: "25%",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 70
+      }
+    },
+    hidden: { marginLeft: "-55%" }
+  }
+
+  const dkDiv =  {
+    visible: {
+      marginLeft: "25%",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 70
+      }
+    },
+    hidden: { marginLeft: "110%"}
+  }
   return (
     <div className="contenido">
-      <div className="transition-div" id="sb-div">
+      <motion.div 
+        className="transition-div" id="sb-div"
+        initial="hidden"
+        animate="visible"
+        variants={sbDiv}
+        >
         <Link to="/sb" className="text-link">
           <SBLogo />
         </Link>
-      </div>
-      <div className="transition-div" id="dk-div">
+      </motion.div>
+      <motion.div 
+        className="transition-div" id="dk-div"
+        initial="hidden"
+        animate="visible"
+        variants={dkDiv}
+        >
         <Link to="/denmark" className="text-link">
           <p>Dinamarca</p>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 }
